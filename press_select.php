6d498e9db1b5f39dbf3ee13dff3c9c952abdbf49
tkfils <tkfils@users.noreply.github.com>
2016-07-21 13:55:13 +0100

    Add files via upload

--- press_select.php
+++ press_select.php
@@ -42,11 +42,8 @@ $date_select .= "%";
 $body = "";
 $check_title="";
 
-#$result = mysql_query("SELECT i.title, i.description, i.pubdate, i.id, i.author, i.url, i.added, i.selections, c.title, i.comments FROM item i, channels c WHERE i.cid=c.id and (c.parent=16 or c.parent=24) and i.added like '".$date_select."' order by i.added desc",$db);
 
-#$result = mysql_query("SELECT i.title, i.description, i.pubdate, i.id, i.author, i.url, i.added, i.selections, c.title, i.comments FROM item i, channels c WHERE i.cid=c.id and i.added like '".$date_select."' and DATE_SUB(CURDATE(), INTERVAL 5 DAY) <= i.pubdate and (c.id=294 or c.id=424 or c.id=425 or c.id=426 or c.id=427 or c.id=428 or c.id=1066 or c.id=1084 or c.id=1085 or c.id=1086 or c.id=1087 or c.id=1088 or c.id=1089 or c.id=1090 or ((c.parent=16 or c.parent=24 or c.id=294 or c.id=309 or c.id=386 or c.id=311 or c.id=312 or c.id=305 or c.id=323) and (i.description like '%King%s Fund%' or i.description like '%Health manager%' or i.description like '%Clinical commissioning%' or i.description like '%GP commissioning%' or i.description like '%GP consortia%' or i.description like '%NHS%' or i.description like '%Health Bill%' or i.description like '%Integrated health%' or i.description like '%Integrated health%' or i.description like '%social care%' or i.description like '%Integrated care%' or i.description like '%ospital%' or i.description like '%patient%' or i.description like '%Doctor%' or i.description like '%nurse%' or i.description like '%health%' or i.description like '%women in leadership%'))) order by i.title asc",$db);
-
-$result = mysql_query("SELECT i.title, i.description, i.pubdate, i.id, i.author, i.url, i.added, i.selections, c.title, i.comments FROM item i, channels c WHERE i.cid=c.id and i.added like '".$date_select."' and DATE_SUB(CURDATE(), INTERVAL 5 DAY) <= i.pubdate and (c.id=294 or c.id=424 or c.id=425 or c.id=426 or c.id=427 or c.id=428 or c.id=1066 or c.id=1084 or c.id=1085 or c.id=1086 or c.id=1087 or c.id=1088 or c.id=1089 or c.id=1090 or ((c.parent=16 or c.parent=24 or c.id=294 or c.id=309 or c.id=386 or c.id=311 or c.id=312 or c.id=305 or c.id=323) and (i.description like '%King%s Fund%' or i.description like '%Health manager%' or i.description like '%Clinical commissioning%' or i.description like '%GP commissioning%' or i.description like '%GP consortia%' or i.description like '%NHS%' or i.description like '%Health Bill%' or i.description like '%Integrated health%' or i.description like '%Integrated health%' or i.description like '%social care%' or i.description like '%Integrated care%' or i.description like '%ospital%' or i.description like '%patient%' or i.description like '%Doctor%' or i.description like '%nurse%' or i.description like '%health%' or i.description like '%women in leadership%' or i.description like '%Health Foundation%' or i.description like '%Anita Charlesworth%' or i.description like '%Jennifer Dixon%' or i.description like '%Nuffield Trust%' or i.description like '%Nigel Edwards%' or i.description like '%Candace Imison%'))) order by i.title asc",$db);
+$result = mysql_query("SELECT i.title, i.description, i.pubdate, i.id, i.author, i.url, i.added, i.selections, c.title, i.comments FROM item i, channels c WHERE i.cid=c.id and i.added like '".$date_select."' and DATE_SUB(CURDATE(), INTERVAL 5 DAY) <= i.pubdate and (c.id=294 or c.id=424 or c.id=425 or c.id=426 or c.id=427 or c.id=428 or c.id=1066 or c.id=1084 or c.id=1085 or c.id=1086 or c.id=1087 or c.id=1088 or c.id=1089 or c.id=1090 or ((c.parent=16 or c.parent=24 or c.id=294 or c.id=309 or c.id=386 or c.id=311 or c.id=312 or c.id=305 or c.id=323) and (i.description like '%King%s Fund%' or i.description like '%Health manager%' or i.description like '%Clinical commissioning%' or i.description like '%GP commissioning%' or i.description like '%NHS%' or i.description like '%ntegrated health%' or i.description like '%social care%' or i.description like '%ntegrated care%' or i.description like '%ospital%' or i.description like '%patient%' or i.description like '%Doctor%' or i.description like '%nurse%' or i.description like '%health%' or i.description like '%Health Foundation%' or i.description like '%Anita Charlesworth%' or i.description like '%Jennifer Dixon%' or i.description like '%Nuffield Trust%' or i.description like '%Nigel Edwards%' or i.description like '%John Appleby%' or i.description like '%Candace Imison%'))) order by i.title asc",$db);
 
 $num=mysql_numrows($result);
 ob_start("callback");
@@ -71,6 +68,7 @@ if (($row[0] <> "") and ($row[0] <> "-")) 	{
 $title=$row[0];
 $title=str_replace("King's Fund","<span style=background-color:red>King's Fund</span>",$title);
 $title=str_replace('Chris Ham','<span style=background-color:red>Chris Ham</span>',$title);
+$title=str_replace('Richard Murray','<span style=background-color:red>Richard Murray</span>',$title);
 $title=str_replace('Anna Dixon','<span style=background-color:red>Anna Dixon</span>',$title);
 $title=str_replace('John Appleby','<span style=background-color:red>John Appleby</span>',$title);
 $title=str_replace('Richard Humphries','<span style=background-color:red>Richard Humphries</span>',$title);
@@ -79,9 +77,7 @@ $title=str_replace('Nick Goodwin','<span style=background-color:red>Nick Goodwin
 $title=str_replace('health manager','<span style=background-color:yellow>health manager</span>',$title);
 $title=str_replace('clinical commissioning','<span style=background-color:yellow>clinical commissioning</span>',$title);
 $title=str_replace('GP commissioning','<span style=background-color:yellow>GP commissioning</span>',$title);
-$title=str_replace('GP consortia','<span style=background-color:yellow>GP consortia</span>',$title);
 $title=str_replace('NHS reform','<span style=background-color:yellow>NHS reform</span>',$title);
-$title=str_replace('Health Bill','<span style=background-color:yellow>Health Bill</span>',$title);
 $title=str_replace('Integrated health','<span style=background-color:yellow>Integrated health</span>',$title);
 $title=str_replace('NHS commissioning board','<span style=background-color:yellow>NHS commissioning board</span>',$title);
 $title=str_replace('integrated health','<span style=background-color:yellow>integrated health</span>',$title);
@@ -126,6 +122,7 @@ if (($row[1] <> "") and ($row[1] <> "-")) 	{
 $description = preg_replace("/<img[^>]+\>/i", " ", $row[1]);
 $description=str_replace("King's Fund","<span style=background-color:red>King's Fund</span>",$description);
 $description=str_replace('Chris Ham','<span style=background-color:red>Chris Ham</span>',$description);
+$description=str_replace('Richard Murray','<span style=background-color:red>Richard Murray</span>',$description);
 $description=str_replace('Anna Dixon','<span style=background-color:red>Anna Dixon</span>',$description);
 $description=str_replace('John Appleby','<span style=background-color:red>John Appleby</span>',$description);
 $description=str_replace('Richard Humphries','<span style=background-color:red>Richard Humphries</span>',$description);
@@ -134,9 +131,7 @@ $description=str_replace('Nick Goodwin','<span style=background-color:red>Nick G
 $description=str_replace('health manager','<span style=background-color:yellow>health manager</span>',$description);
 $description=str_replace('clinical commissioning','<span style=background-color:yellow>clinical commissioning</span>',$description);
 $description=str_replace('GP commissioning','<span style=background-color:yellow>GP commissioning</span>',$description);
-$description=str_replace('GP consortia','<span style=background-color:yellow>GP consortia</span>',$description);
 $description=str_replace('NHS reform','<span style=background-color:yellow>NHS reform</span>',$description);
-$description=str_replace('Health Bill','<span style=background-color:yellow>Health Bill</span>',$description);
 $description=str_replace('Integrated health','<span style=background-color:yellow>Integrated health</span>',$description);
 $description=str_replace('NHS commissioning board','<span style=background-color:yellow>NHS commissioning board</span>',$description);
 $description=str_replace('integrated health','<span style=background-color:yellow>integrated health</span>',$description);
